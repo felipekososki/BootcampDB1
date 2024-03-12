cfb4d4e70d21c42d179277d2973d0f0ab318a746
felipekososki <kososki@gmail.com>
2024-03-12 19:57:17 -0300

    feat: implement code to get number greater 20

--- maioresque20.js
+++ maioresque20.js
@@ -2,10 +2,14 @@ const array = [9, 4, 5, 10, 2, 90, 8, 6, 33]
 
 console.log("Números maiores que 20:");
 
-for (let i = 0; i < array.length; i++) {
-    if (array[i] > 20) {
-        console.log(array[i]);
-    }
-}
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] > 20) {
+//         console.log(array[i]);
+//     }
+// }
 
 // depois fazer igual o pares, criando function
+
+const maiores20 = array.filter(number => number > 20)
+
+console.log("Números maiores que 20:", maiores20)
