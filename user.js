f0270b2929fd068831ded5ed4e272a81e54cebb1
felipekososki <kososki@gmail.com>
2024-03-12 20:32:58 -0300

    feat: create user

---
+++ user.js
@@ -0,0 +1,15 @@
+const user = {
+    name: "Claudia",
+    city: "Campo Mourão",
+    work: "developer",
+    hobbie: "play games"
+}
+
+console.log("Nome: ", 
+    user.name, 
+    "Cidade: ", 
+    user.city, 
+    "Work: ", 
+    user.work, 
+    "Hobbie: ", 
+    user.hobbie);
