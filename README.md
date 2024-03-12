aed28499fc95c96ef36c1685a18f798882c973db
felipekososki <kososki@gmail.com>
2024-03-11 22:05:54 -0300

    criando arquivo exer.js, alteracao readme

--- README.md
+++ README.md
@@ -9,3 +9,10 @@
     - `git status`
 
 - Primeiros comandos
+
+
+## Primeiro Exercício
+ - Criar um arquivo que dado o array de valores [2, 4, 3, 1, 6, 7, 9], retorne quais são pares. Fazer em JavaScript
+ - Alterar o arquivo README.md
+ - Gerar o commit
+ - Enviar para o GitHub
---
+++ exer.js
@@ -0,0 +1,14 @@
+// - Criar um arquivo que dado o array de valores [2, 4, 3, 1, 6, 7, 9], retorne quais são pares. Fazer em JavaScript
+// - Alterar o arquivo README.md
+// - Gerar o commit
+// - Enviar para o GitHub
+
+const array = [2, 4, 3, 1, 6, 7, 9];
+
+function par(n) {
+    return n % 2 === 0;
+}
+
+const pares = array.filter(par);
+
+console.log("valores pares:", pares);
