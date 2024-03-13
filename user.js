8e4e784785a2dae8a4826b6dcf613f02e71e1bc7
felipekososki <kososki@gmail.com>
2024-03-12 22:26:34 -0300

    delete: arquivo maiorque20. fetch: s=user.js. insert: soma.js

--- user.js
+++ user.js
@@ -1,15 +1,27 @@
 const user = {
-    name: "Claudia",
-    city: "Campo Mourão",
-    work: "developer",
-    hobbie: "play games"
+    users: [
+        {
+        name: "Claudia",
+        city: "Campo Mourão",
+        work: "developer",
+        hobbie: "play games"
+        },
+        {
+            name: "Felipe",
+            city: "Maringá",
+            work: "BA",
+            hobbie: "play games"
+        }
+    ]
 }
 
-console.log("Nome: ", 
-    user.name, 
-    "Cidade: ", 
-    user.city, 
-    "Work: ", 
-    user.work, 
-    "Hobbie: ", 
-    user.hobbie);
+// console.log("Nome: ", 
+//     user.users.name, 
+//     "Cidade: ", 
+//     user.users.city, 
+//     "Work: ", 
+//     user.users.work, 
+//     "Hobbie: ", 
+//     user.users.hobbie);
+
+console.log(user)
